5c777f21b9672f2b610cbeb0db1060e7326ee05b
Pablo Glez <pgonzalez@datacreativa.rocks>
2018-06-19 21:37:26 -0500

    =Type parameters

---
+++ Making Our Own Type And Typeclasses/type_parameters.hs
@@ -0,0 +1,25 @@
+data Maybe a = Nothing | Just a
+
+--we could change our car data type from this
+
+data Car = Car { company :: String
+               , model :: String
+               , year :: Int
+               } deriving (Show)
+
+-- to this
+
+data Car a b c = Car { company :: a  
+                     , model :: b  
+                     , year :: c   
+                     } deriving (Show)
+
+--but would we really benefit?
+
+tellCar :: Car -> String
+tellCar (Car {company = c, model = m, year = y}) = "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
+
+--if car was car a b c:
+
+tellCar :: (Show a) => Car String String a -> String
+tellCar (Car {company = c, model = m, year = y}) = "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
