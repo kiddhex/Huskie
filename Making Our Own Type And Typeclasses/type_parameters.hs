cf62148cd9a05ba9a0c9d76a2ce991113b012f2d
kiddhex <pgonzalez@datacreativa.rocks>
2018-07-05 20:04:10 -0500

    Finish type parameters, begin derived instances

--- Making Our Own Type And Typeclasses/type_parameters.hs
+++ Making Our Own Type And Typeclasses/type_parameters.hs
@@ -9,10 +9,10 @@ data Car = Car { company :: String
 
 -- to this
 
-data Car a b c = Car { company :: a  
-                     , model :: b  
-                     , year :: c   
-                     } deriving (Show)
+-- data Car a b c = Car { company :: a  
+--                      , model :: b  
+--                      , year :: c   
+--                      } deriving (Show)
 
 --but would we really benefit?
 
@@ -21,7 +21,20 @@ tellCar (Car {company = c, model = m, year = y}) = "This " ++ c ++ " " ++ m ++ "
 
 --if car was car a b c:
 
-tellCar :: (Show a) => Car String String a -> String
-tellCar (Car {company = c, model = m, year = y}) = "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
+-- tellCar :: (Show a) => Car String String a -> String
+-- tellCar (Car {company = c, model = m, year = y}) = "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
 
 --Don't put type constraints into data declarations even if it makes sense. You'll end up putting it in functions that need it anyway.
+
+--3d vector type:
+
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num t) => Vector t -> Vector t -> Vector t
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+vectorMult :: (Num t) => Vector t -> t -> Vector t
+(Vector i j k) `vectorMult` m = Vector (i*m) (j*m) (k*m)
+
+scalarMult :: (Num t) => Vector t -> Vector t -> t
+(Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
