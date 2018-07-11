72a72ff4a0083a44693a4e18665407fcb88eeaa6
kiddhex <pgonzalez@datacreativa.rocks>
2018-07-10 20:34:39 -0500

    Add binary search tree

--- Making Our Own Type And Typeclasses/recursive_data_structures.hs
+++ Making Our Own Type And Typeclasses/recursive_data_structures.hs
@@ -3,6 +3,18 @@ data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
 --or record syntax:
 --data List a = Empty | Cons { listHead :: a, listTail :: List a} deriving (Show, Read, Eq, Ord)
 
---fixit:
---infixr 5 :-:
---data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+-- fixit:
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+--this is how ++ is defined for normal lists:
+-- infixr 5 ++
+-- (++) :: [a] -> [a] -> [a]
+-- []     ++ ys = ys
+-- (x:xs) ++ ys = x : (xs ++ ys)
+
+--For our list:
+infixr 5 .++
+(.++) :: List -> List -> List
+Empty      .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
