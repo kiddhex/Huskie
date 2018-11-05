f19e501891ebd75d9042ea47c0c8e875bab2e21b
Pablo Glez <pgonzalez@datacreativa.rocks>
2018-11-04 18:25:47 -0600

    Data.Map done

--- Modules/data_map.hs
+++ Modules/data_map.hs
@@ -41,4 +41,37 @@ findKey key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
 
 import qualified Data.Map as Map
 
+--Here's a basic rundown of its functions:
+
+--1. fromList:
+--Map.fromList :: (Ord k) => [(k,v)] -> Map.Map k v
+
+--It needs keys to be orderable so it can arrange them in a tree
+--You should always use Data.Map for key-value associations unless you have keys which can't
+--be ordered.
+
+--2. empty represents an empty map. It takes no arguments, it just returns an empty map.
+--3. insert takes a key, a value and a map and returns a new map with the new values inserted:
+--Map.insert 3 100 Map.empty
+--fromList [(3, 100)]
+
+--We can implement our own fromList using the empty map, insert and a fold:
+
+fromList' :: (Ord k) => [(k,v)] -> Map.Map k v
+fromList' = foldr (\(k,v) acc -> Map.insert k v acc) Map.empty
+
+--4. null checks if a map is empty.
+--5. size reports the size of a map
+--6. singleton takes a key and a value and creates a map that has exactly one mapping.
+--7. lookup works like the Data.List lookup, only it operates on maps. It returns Just something
+--if it finds something for the key and Nothing if it doesn't.
+--8. member is a predicate that takes a key and a map and reports whether the key is in the map
+--or not.
+--9. map and filter work much like their list equivalents.
+--10. toList is the inverse of fromList
+--11. keys and elems return list of keys and values respectively
+--12. fromListWith acts like fromList only it doesn't discard duplicate keys but it uses a function
+--supplied to it to decide what to do with them. 
+--13. insertWith is the equivalent
+
 
