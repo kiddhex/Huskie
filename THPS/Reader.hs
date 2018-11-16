f0ae88b7675c94649268bbcb28262618c9ddf32d
kiddhex <pgonzalez@datacreativa.rocks>
2018-11-16 14:45:45 -0600

    Removed unnecessary return

--- THPS/Reader.hs
+++ THPS/Reader.hs
@@ -4,8 +4,6 @@ main = do
         contents <- readFile "file.txt"
         let splitIntoLinesList = lines contents
         let filtered = filter (isInfixOf "needle") splitIntoLinesList
-        let firstNCharacters = map (take firstNCharacters) filtered
+        let firstNCharacters = map (take 0) filtered
         let stringify = foldl (\acc x -> acc ++ "/" ++ x) "" firstNCharacters
         writeFile "output.txt" stringify
-
-        return 0
